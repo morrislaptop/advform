c7839e9d6962c1d41b277f600761f60b14ca3254
Craig Morris <craig.michael.morris@gmail.com>
2009-09-10 18:18:28 +1000

    Allowed to specify css contexts for the wysiwygs - useful for BakedSimple templates.

--- views/helpers/tinymce.php
+++ views/helpers/tinymce.php
@@ -3,6 +3,11 @@ class TinymceHelper extends AppHelper
 {
 	var $helpers = array('Form', 'Javascript', 'Html', 'Advform.Wysiwygpro');
 	var $embedded = false;
+	var $_bodyClasses = array();
+
+	function registerBodyClass($element, $class) {
+		$this->_bodyClasses[$element] = $class;
+	}
 
 	function embed()
 	{
@@ -11,6 +16,10 @@ class TinymceHelper extends AppHelper
 	    }
 	    $this->embedded = true;
 	    $this->Javascript->link('/vendors/tiny_mce/tiny_mce', false);
+	    $this->Javascript->link('/vendors/tinybrowser/tb_tinymce.js.php', false);
+
+	    $body_class = http_build_query($this->_bodyClasses, null, ',');
+
 	    $js = <<<JS
 	tinyMCE.init({
 	    mode: "specific_textareas",
@@ -22,21 +31,24 @@ class TinymceHelper extends AppHelper
 
 	    // Theme options
 	    theme_advanced_buttons1: "pasteword,bold,italic,|justifyleft,justifycenter,justifyright,|,formatselect,styleselect,removeformat,|,bullist,numlist,|,outdent,indent,blockquote,|,link,unlink,anchor,image,|,table,charmap,code",
-	  theme_advanced_buttons2: "",
-	  theme_advanced_buttons3: "",
+		theme_advanced_buttons2: "",
+		theme_advanced_buttons3: "",
 	    theme_advanced_toolbar_location: "top",
 	    theme_advanced_toolbar_align: "left",
 	    theme_advanced_statusbar_location: "bottom",
 	    theme_advanced_resizing: true,
 	    theme_advanced_resize_horizontal: false,
-	  theme_advanced_path: true,
+		theme_advanced_path: true,
 	    width: '100%',
 
 	    // File manager
-	    file_browser_callback: "TinyMceOpenFileBrowser",
+	    file_browser_callback: "tinyBrowser",
 
 	    // Which textareas?
 	    editor_selector: "tinymce",
+
+	    // Context CSS
+	    body_class: "{$body_class}",
 
 	    // URLs
 	    relative_urls: false,
